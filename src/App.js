7df5771e6459e08056ad960e988a2c135077de47
nickdas1 <75402464+nickdas1@users.noreply.github.com>
2022-05-29 21:16:25 -0400

    add translation widget

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Accordion from "./components/Accordion";
 import Dropdown from "./components/Dropdown";
 import Search from "./components/Search";
+import Translate from "./components/Translate";
 
 const items = [
     {
@@ -40,11 +41,13 @@ function App() {
         <div>
             {/* <Accordion items={items} /> */}
             {/* <Search /> */}
-            <Dropdown
+            {/* <Dropdown
+                label="Select a Color"
                 options={options}
                 selected={selected}
                 onSelectedChange={setSelected}
-            />
+            /> */}
+            <Translate />
         </div>
     );
 }
